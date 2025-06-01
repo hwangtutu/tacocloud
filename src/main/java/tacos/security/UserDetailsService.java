883f5af72b766d5665b6e33a5cf101384ce1451b
hwang <hwangtutu@gmail.com>
2025-06-02 07:15:31 +0900

    Revert "first commit"
    This reverts commit 58792051435bff84099a21f0b8183d9f66233994.

--- src/main/java/tacos/security/UserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package tacos.security;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-public interface UserDetailsService {
-    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-}
